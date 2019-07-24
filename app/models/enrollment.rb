d7339fdd7c984fba265b83d30634a413493e6c19
Spencer <spenjlee@gmail.com>
2019-07-23 18:17:48 -0700

    Add associations to Enrollment for User and Course

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,11 @@
 class Enrollment < ApplicationRecord
+	has_one :course,
+		primary_key: :course_id,
+		foreign_key: :id,
+		class_name:  :Course
+
+	has_one :user,
+		primary_key: :student_id,
+		foreign_key: :id,
+		class_name:  :User
 end
